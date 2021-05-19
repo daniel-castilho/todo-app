971740da6411bd1fd6fb5584d1831cd2d04aee3d
Daniel Castilho <dan.castilho@gmail.com>
2021-05-19 13:59:45 -0400

    Removing Warnings from mongoDB connection

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect('mongodb://localhost/todo');
+
+module.exports = mongoose.connect("mongodb://localhost:27017/todo", {
+	useMongoClient: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
