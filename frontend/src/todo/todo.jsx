319f65204af40e7085e72fedf4cda32619abb89f
Daniel Castilho <dan.castilho@gmail.com>
2021-05-20 11:42:36 -0400

    Delete and Retrieve

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,6 +14,19 @@ export default class Todo extends Component {
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleAdd = this.handleAdd.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+
+		this.refresh();
+	}
+
+	refresh() {
+		axios.get(`${URL}?sort=-createdAt`).then((res) =>
+			this.setState({
+				...this.state,
+				description: "",
+				list: res.data,
+			})
+		);
 	}
 
 	handleChange(e) {
@@ -22,8 +35,11 @@ export default class Todo extends Component {
 
 	handleAdd() {
 		const description = this.state.description;
-		axios.post(URL, { description })
-			.then((res) => console.log("Funcionou!"));
+		axios.post(URL, { description }).then((res) => this.refresh());
+	}
+
+	handleRemove(todo) {
+		axios.delete(`${URL}/${todo._id}`).then((res) => this.refresh());
 	}
 
 	render() {
@@ -35,7 +51,7 @@ export default class Todo extends Component {
 					handleChange={this.handleChange}
 					handleAdd={this.handleAdd}
 				/>
-				<TodoList />
+				<TodoList list={this.state.list} handleRemove={this.handleRemove} />
 			</div>
 		);
 	}
