3df5eb5fd2dba54f24a66e0c203846513f6b123a
Daniel Castilho <dan.castilho@gmail.com>
2021-05-20 10:57:56 -0400

    Integration Backend - Add Event

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import PageHeader from "../template/pageHeader";
 import TodoForm from "./todoForm";
 import TodoList from "./todoList";
+
+const URL = "http://localhost:3000/api/todos";
 
 export default class Todo extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { description: "", list: [] };
 
-        this.handleChange = this.handleChange.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 		this.handleAdd = this.handleAdd.bind(this);
 	}
 
@@ -17,8 +21,10 @@ export default class Todo extends Component {
 	}
 
 	handleAdd() {
-        console.log(this.state.description)
-    }
+		const description = this.state.description;
+		axios.post(URL, { description })
+			.then((res) => console.log("Funcionou!"));
+	}
 
 	render() {
 		return (
@@ -26,8 +32,9 @@ export default class Todo extends Component {
 				<PageHeader name="Tarefas" small="Cadastro"></PageHeader>
 				<TodoForm
 					description={this.state.description}
-                    handleChange={this.handleChange}
-					handleAdd={this.handleAdd} />
+					handleChange={this.handleChange}
+					handleAdd={this.handleAdd}
+				/>
 				<TodoList />
 			</div>
 		);
